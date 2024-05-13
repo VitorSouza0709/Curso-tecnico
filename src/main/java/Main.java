8e6724e7b60f0ddd6bbffe967c282d0475a8772e
Vitor <169068391+VitorSouza0709@users.noreply.github.com>
2024-05-13 22:36:00 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Desenvolvimento de sistemas");
+    System.out.println("");
+    System.out.println("o curso de Desenvolvimento de sistemas visa formar profissionais capacitados para projetar, implementar e manter sistemas computacionais que atendam ás necessidades de organizações e indivíduos.");
+    System.out.println("");
+    System.out.println("O curso abrange diversos  tópicos, incluindo:");
+    System.out.println("");
+    System.out.println("Análise e Projeto de Sistemas");
+    System.out.println("Engenharia de Software");
+    System.out.println("Banco de Dados");
+    System.out.println("Entre outros");
+    System.out.println("");
+    System.out.println("O curso de Desenvolvimento de Sistemas é uma excelente opção para quem deseja seguir carreira na área de tecnologia da informação. Oferece uma formação abrangente e prepara os alunos para enfrentar os desafios do mercado de trabalho em constante evolução.");
+    
+    
+  }
+
+}
+
+ 
